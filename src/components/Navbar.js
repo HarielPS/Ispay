7860ba486984da6cabd7978d52736a6c52ce24d8
Hariel <hpadillas1700@alumno.ipn.mx>
2024-09-04 18:39:58 -0600

    #7 fixed navbar issue and absolute on item

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,29 +5,30 @@ import { Menubar } from "primereact/menubar";
 import { InputText } from "primereact/inputtext";
 import { Badge } from "primereact/badge";
 import { Avatar } from "primereact/avatar";
-import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation"; 
 
-//options on navbar
+// options on navbar
 import { any } from "./helpers/any.navbar";
 import { company } from "./helpers/company.navbar";
 
-
 export default function Navbar() {
-  const router = useRouter();
+  const pathname = usePathname(); 
   const [menuItems, setMenuItems] = useState([]);
+
   const handleChangeRoute = (route) => {
-    router.push(route);
+    if (pathname) {
+      window.location.href = route; 
+    }
   };
+
   const mapItems = (items, handleChangeRoute) => {
     return items.map((item) => {
       if (item.items) {
-        // Si el item tiene sub-items, recursivamente aplica el mapeo
         return {
           ...item,
           items: mapItems(item.items, handleChangeRoute),
         };
       }
-      // Si el item tiene una ruta, agrega el command
       return {
         ...item,
         command: item.route
@@ -37,33 +38,21 @@ export default function Navbar() {
     });
   };
 
-  useEffect(()=> {console.log("ruta:" + router.pathname)},[])
   useEffect(() => {
-    switch (router.pathname) {
-      case "/":
-        setMenuItems(mapItems(any, handleChangeRoute));
-        break;
-      case "/company":
-        setMenuItems(mapItems(company, handleChangeRoute));
-        break;
-      default:
-        setMenuItems(mapItems(any, handleChangeRoute));
-        break;
-    }
-  }, [router.pathname]);
+    if (pathname) {
+      console.log("ruta actual:", pathname);
 
-  /* const itemRenderer = (item) => (
-    <a className="flex align-items-center p-menuitem-link">
-      <span className={item.icon} />
-      <span className="mx-2">{item.label}</span>
-      {item.badge && <Badge className="ml-auto" value={item.badge} />}
-      {item.shortcut && (
-        <span className="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">
-          {item.shortcut}
-        </span>
-      )}
-    </a>
-  ); */
+      if (pathname === "/") {
+        setMenuItems(mapItems(any, handleChangeRoute));
+      } else if (pathname.startsWith("/company")) {
+        setMenuItems(mapItems(company, handleChangeRoute));
+      } else {
+        setMenuItems(mapItems(any, handleChangeRoute));
+      }
+    } else {
+      console.log("pathname no disponible aún");
+    }
+  }, [pathname]); 
 
   const start = (
     <img
@@ -71,7 +60,7 @@ export default function Navbar() {
       src="https://primefaces.org/cdn/primereact/images/logo.png"
       height="40"
       className="mr-2"
-    ></img>
+    />
   );
 
   const end = (
@@ -90,7 +79,7 @@ export default function Navbar() {
 
   return (
     <div style={{ width: "100vw", overflow: "hidden" }}>
-      <div className="card">
+      <div className="card" style={{ width: '100%', position: 'absolute' }}>
         <Menubar model={menuItems} start={start} end={end} />
       </div>
     </div>
