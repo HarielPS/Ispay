54d919f955b7f185a68e88e0ac6f7817616cdf94
marlon <marlonrodriguez2b@gmail.com>
2024-09-06 03:31:35 -0600

    build: Models and queries to Firebase, sign up logic queries working

--- src/lib/wallet/Wallet.lib.js
+++ src/lib/wallet/Wallet.lib.js
@@ -1,18 +1,27 @@
 import { ethers } from "ethers";
 
 class Wallet {
-  static methods = {};
-  constructor() {}
-}
-
-Wallet.methods.createWallet = () => {
-  const wallet = ethers.Wallet.createRandom();
-  const address = wallet.address;
-  const privateKey = wallet.privateKey;
-  return {
-    address,
-    privateKey,
+  constructor(address = null, privateKey = null, balance = null) {
+    if (address && privateKey) {
+      this.address = address;
+      this.privateKey = privateKey;
+      this.balance = balance !== null ? balance : 0; // Balance 0 si no se proporciona
+    } else {
+      const { address: newAddress, privateKey: newPrivateKey } =
+        this.createWallet();
+      this.address = newAddress;
+      this.privateKey = newPrivateKey;
+      this.balance = 0; // Balance por defecto para nuevas wallets
+    }
+  }
+  // Generar nueva wallet
+  createWallet = () => {
+    const wallet = ethers.Wallet.createRandom();
+    return {
+      address: wallet.address,
+      privateKey: wallet.privateKey,
+    };
   };
-};
+}
 
 export default Wallet;
--- src/services/Firebase/Firebase.queries.js
+++ src/services/Firebase/Firebase.queries.js
@@ -1,4 +1,33 @@
-import fb from "./Firebase";
-
+import { doc, setDoc, collection, addDoc } from "firebase/firestore";
+import { db } from "./Firebase";
 
 export const FBQueries = {};
+
+FBQueries.SignupCompany = async (companyData, userData) => {
+  const docID = companyData.ID_company_tax;
+  try {
+    // Crear referencia al documento de la empresa
+    const docRef = doc(db, "EMPRESAS", docID);
+    await setDoc(docRef, {});
+
+    // Agregar la colección de usuarios dentro del documento de empresa creado
+    const userCollectionRef = collection(db, "EMPRESAS", docID, "USUARIO");
+    await addDoc(userCollectionRef, userData);
+
+    // Agregar la colección de empresa dentro del documento de empresa creado
+    const empresaCollectionRef = collection(db, "EMPRESAS", docID, "EMPRESA");
+    await addDoc(empresaCollectionRef, companyData);
+
+    return {
+      success: true,
+      message: "Empresa y usuario registrados correctamente",
+    };
+  } catch (error) {
+    console.log("Error al crear el documento: ", error);
+
+    return {
+      success: false,
+      message: `Error al registrar la empresa: ${error.message}`,
+    };
+  }
+};
